547b3eb9783b6c954dc32e1a491b96e747251bae
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-09-10 12:20:24 +0530

    Update ReceiverHeader.h

--- Receiver/ReceiverHeader.h
+++ Receiver/ReceiverHeader.h
@@ -8,7 +8,13 @@ class Receiver
 {
 private:
 	vector<vector<int>> footfallData;
+	int date;
+	int month;
+	int year;
+	float hourlyAverage;
 public:
+	Receiver();
+	Receiver(int, int, int, float);
 	void readSenderData();
 	void storeFootfallData(const string&);
 	void averageFootfallsPerHourDaily();
