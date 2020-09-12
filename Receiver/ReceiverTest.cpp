25d9341591868038d2295f3fdb7452c43fbc6a71
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-09-12 07:57:39 +0530

    Update

--- Receiver/ReceiverTest.cpp
+++ Receiver/ReceiverTest.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
-#include "catch.hpp"
 #include "ReceiverHeader.h"
+#include "catch.hpp"
+
 using namespace std;
 TEST_CASE("Average footfalls per hour day wise") {
     Receiver obj;
